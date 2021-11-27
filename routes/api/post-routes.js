6e0415fda98c296db919582b3d4dcd948a8d786f
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-27 15:47:13 -0600

    added Vote model and added belongsToMany relationship as applicable

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { route } = require('.');
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('====================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -22,12 +23,13 @@ router.get('/', (req, res) => {
     });
 });
 
+// get single user
 router.get('/:id', (req, res) => {
     Post.findOne({
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         include: [
             {
                 model: User,
@@ -48,6 +50,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// create new post
 router.post('/', (req, res) => {
     Post.create({
         title: req.body.title,
@@ -58,6 +61,16 @@ router.post('/', (req, res) => {
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
+    });
+});
+
+// Update existing post
+router.put('/upvote', (req, res) => {
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
@@ -85,6 +98,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete existing post
 router.delete('/:id', (req, res) => {
     Post.destroy({
         where: {
